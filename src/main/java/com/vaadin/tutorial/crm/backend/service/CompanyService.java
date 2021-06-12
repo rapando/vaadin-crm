999541b80496356751e8786103f84c60bf334b27
rapando <samsonrapando@gmail.com>
2021-06-12 14:15:46 +0300

    done creating form

--- src/main/java/com/vaadin/tutorial/crm/backend/service/CompanyService.java
+++ src/main/java/com/vaadin/tutorial/crm/backend/service/CompanyService.java
@@ -2,9 +2,11 @@ package com.vaadin.tutorial.crm.backend.service;
 
 import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.repository.CompanyRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CompanyService {
     private CompanyRepository companyRepository;
 
--- src/main/java/com/vaadin/tutorial/crm/ui/forms/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/forms/ContactForm.java
@@ -1,5 +1,7 @@
 package com.vaadin.tutorial.crm.ui.forms;
 
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -10,6 +12,8 @@ import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
 
@@ -21,13 +25,12 @@ public class ContactForm extends FormLayout {
     EmailField email = new EmailField("Email");
     ComboBox<Contact.Status> status = new ComboBox<>("Status");
     ComboBox<Company> company = new ComboBox<>("Company");
-
     Button save = new Button("Save");
     Button delete = new Button("Delete");
     Button close = new Button("Cancel");
-
     // create binder for form data
     Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+    private Contact contact;
 
 
     public ContactForm(List<Company> companies) { // list of companies is needed for the dropdown
@@ -37,7 +40,7 @@ public class ContactForm extends FormLayout {
         company.setItems(companies);
         company.setItemLabelGenerator(Company::getName); // this function returns the name of the company
         status.setItems(Contact.Status.values());
-        add (firstName, lastName, email, company, status, createButtonsLayout());
+        add(firstName, lastName, email, company, status, createButtonsLayout());
     }
 
     private HorizontalLayout createButtonsLayout() {
@@ -48,6 +51,66 @@ public class ContactForm extends FormLayout {
         save.addClickShortcut(Key.ENTER);
         close.addClickShortcut(Key.ESCAPE);
 
+        // add button events
+        save.addClickListener(event -> validateAndSave());
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, contact)));
+        close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
         return new HorizontalLayout(save, delete, close);
     }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(contact);
+            fireEvent(new SaveEvent(this, contact));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+        binder.readBean(contact);
+    }
+
+    // register the custom events
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType, ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
+    // Events
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private final Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    // save
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    // Delete
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    // Close
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
 }
+
