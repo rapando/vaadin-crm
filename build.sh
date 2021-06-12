eb15dc65acca6f6d3ab7e661e1728551872d3afa
rapando <samsonrapando@gmail.com>
2021-06-12 16:16:53 +0300

    done for now

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-mvn install -Pproduction
+mvn clean package -Pproduction -DskipTests
 scp target/vaadin-crm-1.0-SNAPSHOT.jar root@innv8.lib.co.ke:/apps/admin/app.jar
--- src/main/java/com/vaadin/tutorial/crm/ui/MainLayout.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainLayout.java
@@ -10,10 +10,19 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.tutorial.crm.ui.view.dashboard.DashboardView;
 import com.vaadin.tutorial.crm.ui.view.list.ListView;
 
 @CssImport("./styles/shared-styles.css")
+@PWA(
+        name = "AdminCRM",
+        shortName = "CRM",
+        offlineResources = {
+                "./styles/offline.css",
+                "./images/offline.png"
+        }
+)
 public class MainLayout extends AppLayout {
     public MainLayout() {
         createHeader();
